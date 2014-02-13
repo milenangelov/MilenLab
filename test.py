95ebc184d39720c533e878bfc2586b4a6fecd22b
pimpf <angue.off@gmail.com>
2014-02-13 15:05:53 +0200

    This is first test

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("Hello, world!")
+#  File:       test.py
+
+def factorial(n): 
+  if n == 0: 
+    return 1 
+  else: 
+    return n * factorial(n-1) 
+
+print (" 5! has a value of: ",)
+result = factorial(5)
+print (result)
+
+print (" 4! has a value of:",)
+result = factorial(4)
+print (result)
+
